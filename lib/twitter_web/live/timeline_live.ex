db01d860bf7c91ebc6804a72fc060a066ad63936
qodot <waitingforqodot@gmail.com>
2025-02-10 23:09:40 +0900

    입력 양식 선언

--- lib/twitter_web/live/timeline_live.ex
+++ lib/twitter_web/live/timeline_live.ex
@@ -3,8 +3,35 @@ defmodule TwitterWeb.TimelineLive do
 
   def render(assigns) do
     ~H"""
-    <div>
-      <h1>Timeline</h1>
+    <div class="px-2">
+      <div class="w-full mx-auto pt-2 flex flex-col gap-2">
+        <div>
+          <input
+            type="text"
+            placeholder="닉네임"
+            class="flex h-10 px-3 py-2 text-sm bg-white border rounded-md border-neutral-300 ring-offset-background placeholder:text-neutral-500 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50"
+          />
+        </div>
+
+        <div class="w-full">
+          <textarea
+            type="text"
+            placeholder="무슨 일이 일어나고 있나요?"
+            class="flex w-full h-auto min-h-[80px] px-3 py-2 text-sm bg-white border rounded-md border-neutral-300 placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50"
+          ></textarea>
+        </div>
+
+        <div class="flex flex-row justify-between items-center">
+          <div></div>
+
+          <button
+            type="button"
+            class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-neutral-950 hover:bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none"
+          >
+            트윗!
+          </button>
+        </div>
+      </div>
     </div>
     """
   end
